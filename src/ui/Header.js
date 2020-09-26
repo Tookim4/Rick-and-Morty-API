4f75232dc4dfa92847b9f21eae435b339e3b82c3
Wolfhound9 <tookim4@gmail.com>
2020-09-26 20:24:17 +0300

    commit bar

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Logo from '../images/pngguru.com.png'
-import Buttons from './Buttons'
 
 const Header = () => {
     return (
         <div className='main-header'>
                 <img src={Logo} alt='brand' className='brand-logo'></img>
-                <Buttons/>
         </div>
     )
 }
